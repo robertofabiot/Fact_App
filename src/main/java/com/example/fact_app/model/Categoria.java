b6dcf609ab03c46d2fcf1147ed86b66852172e97
Roberto F. Tercero <rftercero1@gmail.com>
2026-09-08 16:01:06 -0600

    Update categoria, producto

--- src/main/java/com/example/fact_app/model/Categoria.java
+++ src/main/java/com/example/fact_app/model/Categoria.java
@@ -1,4 +1,19 @@
 package com.example.fact_app.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Categoria {
+    private Integer id;
+    private String nombre;
+    private boolean activa;
+
+    @Override
+    public String toString(){
+        return nombre;
+    }
 }
--- src/main/java/com/example/fact_app/model/Producto.java
+++ src/main/java/com/example/fact_app/model/Producto.java
@@ -1,4 +1,20 @@
 package com.example.fact_app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
+    private String codigo;
+    private String nombre;
+    private Categoria categoria;
+    private BigDecimal precioVenta;
+    private int existencia;
+    private String rutaImagen;
+    private boolean activo;
 }
